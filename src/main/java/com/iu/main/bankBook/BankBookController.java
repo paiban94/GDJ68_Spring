cb3780506dd28770c8f85bdbb70e31209cfbf483
pabian94 <pabian94@naver.com>
2023-07-21 11:07:26 +0900

    0721_01

--- src/main/java/com/iu/main/bankBook/BankBookController.java
+++ src/main/java/com/iu/main/bankBook/BankBookController.java
@@ -1,9 +1,13 @@
 package com.iu.main.bankBook;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 //클래스선언부
 @Controller 
@@ -13,18 +17,20 @@ public class BankBookController {
 	@Autowired
 	private BankBookService bankBookService; //의존적
 
-	@RequestMapping(value = "list.do", method = RequestMethod.GET)
-	public String getList()throws Exception{
-		System.out.println("list");
-		
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public String getList(Model model)throws Exception{
+		List<BankBookDTO> ar=bankBookService.getList();
+		model.addAttribute("list", ar);
 		return "bankbook/list"; // list.jsp 찾아가기
 	}
 	@RequestMapping(value = "detail")
-	public String getDetail(BankBookDTO bankBookDTO)throws Exception{
+	public ModelAndView getDetail(BankBookDTO bankBookDTO, ModelAndView mv)throws Exception{
 	//	bankBookDTO = bankBookService.getDetail(bankBookDTO);
 	//	bankBookDTO=bankBookService.getDetail(bankBookDTO);
 		bankBookDTO = bankBookService.getDetail(bankBookDTO);
 		System.out.println(bankBookDTO.getBookName());
-		return "bankbook/detail";
+		mv.addObject("dto",bankBookDTO);
+		mv.setViewName("bankbook/detail");
+		return mv;
 	}
 }
